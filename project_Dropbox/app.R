f185cadbd0d757e6577af88f4ec54ba01e71c15d
hanspeter6 <h.bakker@ru.ac.za>
2018-03-03 17:43:00 +0200

    after placing reactive function into "server" space it works, although there is an http error until finished writing the input. Will try change by including a "submit" button...

--- project_Dropbox/app.R
+++ project_Dropbox/app.R
@@ -24,20 +24,20 @@ if (interactive()) {
                         sidebarPanel(
                                 
                                 # inputs
-                                # textInput("inputDir", "Input Directory"),
-                                br(),
-                                textInput("outputDir", "Output Directory"),
-                                br(),
-                                textInput("keys", "Key Words") 
+                                textInput("inputDir", "Input Directory")
+                                # br(),
+                                # textInput("outputDir", "Output Directory"),
+                                # br(),
+                                # textInput("keys", "Key Words") 
 
                         ),
                         mainPanel(
-                                textOutput("contents1"),
-                                br(),
-                                textOutput("contents2"),
-                                br(),
-                                textOutput("contents3"),
-                                br(),
+                                # textOutput("contents1"),
+                                # br(),
+                                # textOutput("contents2"),
+                                # br(),
+                                # textOutput("contents3"),
+                                # br(),
                                 textOutput("contents4")
 
                         )
@@ -48,56 +48,57 @@ if (interactive()) {
         # create list of pdf documents read from input directory from remote dropbox as reactive statement
        
         
-        dataInput <- reactive( {
-                
-                # req(input$inputDir)
-                inputDir <- "inputDirectory"
-                
-                token <- readRDS("droptoken.rds")
-                
-                filesInfo <- drop_dir(inputDir, dtoken = token) #list of files in Dropbox /inputDir ("inputDirectory").NB also want to confirm only .pdfs
-                
-                filePaths <- filesInfo$path_display # paths to the files
-                
-                # doc_list <- list()
-                for(i in 1:1) {#
-                        
-                        drop_download(filePaths[i], dtoken = token, overwrite = TRUE) # download each from remote dropbox to current wrkdir
-                        
-                        temp <- pdf_text(filesInfo$name[i]) # list of single character string per pdf page
-                        
-                        file.remove(filesInfo$name[i]) # remove the file that was added
-                        
-                        doc_list <- unlist(strsplit(temp,"\n")) # create list of documents split by line
-                }
-                
-                file.remove("droptoken.rds") # cleaning up local working directory
-                
-                return(doc_list)
-                
-        })
+
         
         server <- function(input, output) {
+                
+                dataInput <- reactive( {
+                        
+                        req(input$inputDir)
+                        # inputDir <- "inputDirectory"
+                        
+                        token <- readRDS("droptoken.rds")
+                        
+                        filesInfo <- drop_dir(as.character(input$inputDir), dtoken = token) #list of files in Dropbox /inputDir ("inputDirectory").NB also want to confirm only .pdfs
+                        
+                        filePaths <- filesInfo$path_display # paths to the files
+                        
+                        doc_list <- list()
+                        for(i in 1:length(filePaths)) {#
+                                
+                                drop_download(filePaths[i], dtoken = token, overwrite = TRUE) # download each from remote dropbox to current wrkdir
+                                
+                                temp <- pdf_text(filesInfo$name[i]) # list of single character string per pdf page
+                                
+                                file.remove(filesInfo$name[i]) # remove the file that was added
+                                
+                                doc_list[[i]] <- unlist(strsplit(temp,"\n")) # create list of documents split by line
+                        }
+                        
+                        file.remove("droptoken.rds") # cleaning up local working directory
+                        
+                        return(doc_list[[2]]) # seems can only return single element of a list in this case!!
+                        
+                })
                
-                output$contents1 <- renderText({
-                
-                        print(input$inputDir)
-                })               
+                # output$contents1 <- renderText({
+                # 
+                #         print(input$inputDir)
+                # })               
 
-                output$contents2 <- renderText({
-                        
-                        print(input$outputDir)
-                })
-                
-                output$contents3 <- renderText({
-                        
-                        print(input$keys)
-                })
-                
+                # output$contents2 <- renderText({
+                #         
+                #         print(input$outputDir)
+                # })
+                # 
+                # output$contents3 <- renderText({
+                #         
+                #         print(input$keys)
+                # })
+                # 
                 output$contents4 <- renderText({
-                        out <- dataInput()
 
-                        print(out)
+                        print(dataInput())
                 })
 
                 
